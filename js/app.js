9a3bcbbbb244e9a6dff5f2f6ef9db4a3953636ac
julioallday <naruto481@gmail.com>
2022-12-03 14:11:15 -0300

    ajustes de processamento de símbolos

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 setClass("palindrome", "palindrome");
 
-
 function handleInput() {
   let el_input = document.getElementById("input");
   checkPalindrome(el_input.value);
@@ -9,15 +8,21 @@ function setClass(id, className) {
   let element = document.getElementById(id);
   element.setAttribute("class", className);
 }
+function changeButtonContent(id, newContent){
+  const el_button = document.getElementById(id);
+  el_button.innerText = newContent.toString();
+}
 function checkPalindrome(inputString) {
   let letras = wordReverse(inputString);
   letras = letras.toString();
   let newWord = removeQuotes(letras);
-  if (validateWords(inputString, newWord)) {
-    setClass("palindrome", "palindromeTrue")
+  if (validateWords(removeSpecials(inputString), newWord)) {
+    setClass("palindrome", "palindromeTrue");
+    changeButtonContent("verify", "Yes, it's a palindrome!")
     return true;
   } else {
-    setClass("palindrome", "palindrome")
+    setClass("palindrome", "palindrome");
+    changeButtonContent("verify", "No, it isn't a palindrome..")
     return false;
   }
 }
@@ -30,15 +35,28 @@ function wordReverse(string) {
 }
 function removeQuotes(string) {
   let word = string.replaceAll(/,/g, "");
-  return word;
+  
+  return removeSpecials(word)
+}
+function removeSpecials(string) {
+  string = string.replaceAll(/[ÀÁÂÃÄÅ]/g, "A");
+  string = string.replaceAll(/[àáâãäå]/g, "a");
+  string = string.replaceAll(/[ÈÉÊË]/g, "E");
+  string = string.replaceAll(/[èéêë]/g, "e");
+  string = string.replaceAll(/[ÌÍ]/g, "I");
+  string = string.replaceAll(/[ìí]/g, "i");
+  string = string.replaceAll(/[Ç]/g, "C");
+  string = string.replaceAll(/[ç]/g, "c");
+  string = string.replaceAll(/[^a-z0-9]/gi, "");
+
+  return string;
 }
 function validateWords(word1, word2) {
   if (word1.length > 0 && word1.length <= 10 ** 5) {
-    if (word1.toUpperCase() === word2.toUpperCase()) {
+    if (word1.toUpperCase() == word2.toUpperCase()) {
       return true;
     }
     false;
   }
   false;
 }
-
