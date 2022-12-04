f504b26ac532e84f73c30dc370c9cdf0277d4dd9
julioallday <naruto481@gmail.com>
2022-12-03 22:47:42 -0300

    .

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 let result;
 let language = {
-  eng: {
+  eng:{
     title: "Palindrome",
     content:
       "Palindrome, is every " +
@@ -12,7 +12,7 @@ let language = {
     verifyFalse: "No, it isn't a palindrome..",
     verifyTrue: "Yes, it's a palindrome",
   },
-  ptBr: {
+  ptBr:{
     title: "Palíndromo",
     content:
       "Palíndromo, é toda " +
@@ -25,17 +25,15 @@ let language = {
     verifyTrue: "Sim, é um palíndromo",
   },
 };
-
-function setClass(id, className) {
+function setClass(id, className){
   let element = document.getElementById(id);
   element.setAttribute("class", className);
 }
-function handleInput() {
+function handleInput(){
   let el_input = document.getElementById("input");
   checkPalindrome(el_input.value);
-  
 }
-function checkPalindrome(inputString) {
+function checkPalindrome(inputString){
   exemple.textContent = "";
   let charList = textReverse(inputString);
   let text = charList.toString();
@@ -43,11 +41,11 @@ function checkPalindrome(inputString) {
   output.textContent = removeSpecials(inputString).toLowerCase();
   isTextsEquals(validateWords(removeSpecials(inputString), newText));
 }
-function changeButtonContent(id, newContent) {
+function changeButtonContent(id, newContent){
   const el_button = document.getElementById(id);
   el_button.innerText = newContent.toString();
 }
-function isTextsEquals(condition) {
+function isTextsEquals(condition){
   
   if (condition) {
     transformStructure(true);
@@ -55,7 +53,7 @@ function isTextsEquals(condition) {
     transformStructure(false);
   }
 }
-function transformStructure(condition) {
+function transformStructure(condition){
   if (condition) {
     setClass("palindrome", "palindromeTrue");
     changeButtonContent("verify", "Yes, it's a palindrome!");
@@ -76,18 +74,18 @@ function transformStructure(condition) {
     }
   }
 }
-function textReverse(string) {
+function textReverse(string){
   const letras = [];
   for (let i = string.length - 1; i >= 0; i--) {
     letras.push(string[i]);
   }
   return letras;
 }
-function removeQuotes(string) {
+function removeQuotes(string){
   let word = string.replaceAll(/,/g, "");
   return word;
 }
-function removeSpecials(string) {
+function removeSpecials(string){
   string = string.replaceAll(/[ÀÁÂÃÄÅ]/g, "A");
   string = string.replaceAll(/[àáâãäå]/g, "a");
   string = string.replaceAll(/[ÈÉÊË]/g, "E");
@@ -100,7 +98,7 @@ function removeSpecials(string) {
   removeQuotes(string);
   return string;
 }
-function validateWords(word1, word2) {
+function validateWords(word1, word2){
   if (word1.length > 0 && word1.length <= 10 ** 5) {
     if (word1.toUpperCase() == word2.toUpperCase()) {
       return true;
@@ -109,27 +107,26 @@ function validateWords(word1, word2) {
   }
   false;
 }
-function changeLanguage(lang) {
+function changeLanguage(lang){
   location.hash = lang;
   location.reload();
 }
-function setExemple() {
-  if (window.location.hash != "#ptBr") {
+function setExemple(){
+  if (window.location.hash != "#ptBr"){
     input.textContent = language.eng.usableExemple;
-  } else if (window.location.hash == "#ptBr") {
+  } else if (window.location.hash == "#ptBr"){
     input.textContent = language.ptBr.usableExemple;
   }
 }
-function resetPage() {
+function resetPage(){
   location.reload();
 }
-function main() {
+function main(){
   title.textContent = language.eng.title;
   description.textContent = language.eng.content;
   exemple.textContent = language.eng.exemple;
   setClass("palindrome", "palindrome");
-  window.location.hash = "#es"
-  if (window.location.hash) {
+  if (window.location.hash){
     if (window.location.hash != "#ptBr") {
       title.textContent = language.eng.title;
       description.textContent = language.eng.content;
@@ -142,5 +139,4 @@ function main() {
     }
   }
 }
-
 main();
